fe9c1651445e4e9de0af7db5848f85922f1700ef
gujiuning <15413926+gujiuning@user.noreply.gitee.com>
2025-02-18 22:10:19 +0800

    继续修改

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# npm install react-image-crop
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,35 +1,32 @@
-import { Route, Router } from 'wouter'; 
+import { Route, Router } from 'wouter';
 import Start from './pages/startPage/start';
 import Login from './pages/login/login';
 import Register from "./pages/register/register";
 import SetMine from "./pages/setMine/setMine";
 import Showcase from './pages/Showcase/Showcase';
 import StartBlessing from './pages/startBlessing/startBlessing';
+
 const MyRouter = () => {
-    const router =[{
-        path:"/", component:Start 
-    },{
-        path:"/login", component:Login
-    },{
-        path:"/register",component:Register 
-    },{
-        path:"/set",component:SetMine
-    },{
-        path:"/showCase",component:Showcase
-    },{
-        path:"/startBlessing",component:StartBlessing
-    }]
+    const router = [
+        { path: "/", component: Start },
+        { path: "/login", component: Login },
+        { path: "/register", component: Register },
+        { path: "/set", component: SetMine },
+        { path: "/showCase", component: Showcase },
+        { path: "/startBlessing", component: StartBlessing }
+    ];
 
     return (
         <Router>
-            {router.map((item)=>{
-                return <Route key={item.path} path={item.path} component={item.component}></Route>
+            {router.map((item) => {
+                return (
+                    <Route key={item.path} path={item.path}>
+                        <item.component />
+                    </Route>
+                );
             })}
         </Router>
-    )
-    
+    );
 }
-      
-export default MyRouter
-      
-    
+
+export default MyRouter;
